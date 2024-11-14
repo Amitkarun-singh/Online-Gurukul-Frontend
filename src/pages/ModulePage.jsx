8ee229b97d356e41cc4c17f6094693f4940870c4
Amit Singh <amitkarun4559@gmail.com>
2024-11-15 00:20:42 +0530

    Added notes parts

--- src/pages/ModulePage.jsx
+++ src/pages/ModulePage.jsx
@@ -34,8 +34,7 @@ const ModulePage = ({ sidebarOpen, user }) => {
 
             {activeTab === 'notes' && (
                 <div className="mt-4">
-                    {/* <h1>Notes</h1> */}
-                    <CreateNotes/>
+                    <CreateNotes user={user}/>
                 </div>
             )}
 
